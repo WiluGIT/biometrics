36b367693f24a07106740df72af858472db62d7f
Konrad Wilczek <wolframowsky@gmail.com>
2020-03-25 04:22:15 +0100

    Fucionality of creating image histogram

---
+++ ImageHistogramWindow.py
@@ -0,0 +1,290 @@
+import os
+import matplotlib.pyplot as plt
+
+from PIL import Image
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QInputDialog, QFileDialog, QLabel, QColorDialog, QApplication, QMessageBox
+from PyQt5.QtGui import QPixmap, QImage, QColor, QPalette, QPainter
+
+class Ui_ImageProcessingWindow(object):
+    clicked_pixel_x = None
+    clicked_pixel_y = None
+    def setupUi(self, ImageProcessingWindow):
+        ImageProcessingWindow.setObjectName("ImageProcessingWindow")
+        ImageProcessingWindow.resize(800, 800)
+        self.centralwidget = QtWidgets.QWidget(ImageProcessingWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.photo = QtWidgets.QLabel(self.centralwidget)
+        self.photo.setGeometry(QtCore.QRect(0, 0, 100, 80))
+        self.photo.setAlignment(QtCore.Qt.AlignCenter)
+        self.photo.setText("")
+        pixmap = QtGui.QPixmap("img/kontrolny3.tiff")
+        # version with quality lose
+        # base_pixmap = pixmap.scaled(100, 80)
+        self.photo.setPixmap(pixmap)
+        self.photo.setScaledContents(True)
+        self.photo.setObjectName("photo")
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(0, 620, 800, 20))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.loadButton = QtWidgets.QPushButton(self.centralwidget)
+        self.loadButton.setGeometry(QtCore.QRect(0, 640, 113, 32))
+        self.loadButton.setObjectName("loadButton")
+        self.saveButton = QtWidgets.QPushButton(self.centralwidget)
+        self.saveButton.setGeometry(QtCore.QRect(0, 680, 113, 32))
+        self.saveButton.setObjectName("saveButton")
+        self.pathLabel = QtWidgets.QLabel(self.centralwidget)
+        self.pathLabel.setGeometry(QtCore.QRect(10, 730, 780, 16))
+        self.pathLabel.setObjectName("pathLabel")
+        self.sizeBox = QtWidgets.QComboBox(self.centralwidget)
+        self.sizeBox.setGeometry(QtCore.QRect(145, 682, 104, 26))
+        self.sizeBox.setObjectName("sizeBox")
+        self.sizeBox.addItem("")
+        self.sizeBox.addItem("")
+        self.sizeBox.addItem("")
+        self.sizeBox.addItem("")
+        self.resizeLabel = QtWidgets.QLabel(self.centralwidget)
+        self.resizeLabel.setGeometry(QtCore.QRect(150, 650, 71, 16))
+        self.resizeLabel.setObjectName("resizeLabel")
+        self.pixelLabel = QtWidgets.QLabel(self.centralwidget)
+        self.pixelLabel.setGeometry(QtCore.QRect(290, 650, 91, 16))
+        self.pixelLabel.setObjectName("pixelLabel")
+        self.pixelPhoto = QtWidgets.QLabel(self.centralwidget)
+        self.pixelPhoto.setGeometry(QtCore.QRect(290, 670, 80, 60))
+        self.pixelPhoto.setObjectName("pixelPhoto")
+        self.pickerButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pickerButton.setGeometry(QtCore.QRect(410, 670, 113, 32))
+        self.pickerButton.setObjectName("pickerButton")
+        self.pixelValueLabel = QtWidgets.QLabel(self.centralwidget)
+        self.pixelValueLabel.setGeometry(QtCore.QRect(420, 650, 91, 16))
+        self.pixelValueLabel.setObjectName("pixelValueLabel")
+        self.valueButton = QtWidgets.QPushButton(self.centralwidget)
+        self.valueButton.setGeometry(QtCore.QRect(410, 700, 113, 32))
+        self.valueButton.setObjectName("valueButton")
+        self.histogramButton = QtWidgets.QPushButton(self.centralwidget)
+        self.histogramButton.setGeometry(QtCore.QRect(530,670,113,32))
+        self.histogramButton.setObjectName("histogramButton")
+        ImageProcessingWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(ImageProcessingWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        ImageProcessingWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(ImageProcessingWindow)
+        self.statusbar.setObjectName("statusbar")
+        ImageProcessingWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(ImageProcessingWindow)
+        QtCore.QMetaObject.connectSlotsByName(ImageProcessingWindow)
+
+        # init size
+        self.size_combo_box()
+        # click events
+        self.loadButton.clicked.connect(self.open_dialog_box)
+        self.sizeBox.currentIndexChanged.connect(self.size_combo_box)
+        self.photo.mousePressEvent = self.get_pixel_position
+        self.pickerButton.clicked.connect(self.show_color_dialog)
+        self.valueButton.clicked.connect(self.save_pixel_value)
+        self.saveButton.clicked.connect(self.save_dialog_box)
+        self.histogramButton.clicked.connect(self.create_histogram)
+
+    def retranslateUi(self, ImageProcessingWindow):
+        _translate = QtCore.QCoreApplication.translate
+        ImageProcessingWindow.setWindowTitle(_translate("ImageProcessingWindow", "Image Processing"))
+        self.loadButton.setText(_translate("ImageProcessingWindow", "Choose File"))
+        self.saveButton.setText(_translate("ImageProcessingWindow", "Save File"))
+        self.pathLabel.setText(_translate("ImageProcessingWindow", "img/kontrolny3.tiff"))
+        self.sizeBox.setItemText(0, _translate("ImageProcessingWindow", "1x"))
+        self.sizeBox.setItemText(1, _translate("ImageProcessingWindow", "2x"))
+        self.sizeBox.setItemText(2, _translate("ImageProcessingWindow", "4x"))
+        self.sizeBox.setItemText(3, _translate("ImageProcessingWindow", "8x"))
+        self.resizeLabel.setText(_translate("ImageProcessingWindow", "Select size:"))
+        self.pixelLabel.setText(_translate("ImageProcessingWindow", "Selected pixel:"))
+        self.pixelValueLabel.setText(_translate("ImageProcessingWindow", "Pixel Value:"))
+        self.valueButton.setText(_translate("ImageProcessingWindow", "Save Value"))
+        self.pickerButton.setText(_translate("ImageProcessingWindow", "Choose Color"))
+        self.histogramButton.setText(_translate("ImageProcessingWindow", "Plot histogram"))
+
+
+    def create_histogram(self):
+        try:
+            image = Image.open(self.pathLabel.text(), "r")
+            image = image.convert("RGB")
+            pix_val = list(image.getdata())
+
+            # check whether image is rgb or grayscale
+            coloured = self.check_coloured(pix_val)
+
+            if not coloured:
+                pix_val_flat = [x for sets in pix_val for x in sets]
+
+                pixel_dic = self.count_pixels_histogram(pix_val_flat)
+                plt.bar(list(pixel_dic.keys()), pixel_dic.values(), color='gray')
+                plt.show()
+            else:
+                r = []
+                g = []
+                b = []
+                for i in range(len(pix_val)):
+                    r.append(pix_val[i][0])
+                    g.append(pix_val[i][1])
+                    b.append(pix_val[i][2])
+
+                r = self.count_pixels_histogram(r)
+                plt.subplot(3, 1, 1)
+                plt.bar(list(r.keys()), r.values(), color='r')
+
+                g = self.count_pixels_histogram(g)
+                plt.subplot(3, 1, 2)
+                plt.bar(list(g.keys()), g.values(), color='g')
+
+                b = self.count_pixels_histogram(b)
+                plt.subplot(3, 1, 3)
+                plt.bar(list(b.keys()), b.values(), color='b')
+
+                plt.show()
+
+                # METHOD WITH BUILT IN FUNCTION
+                # region
+                # r2, g2, b2 = image.split()
+                # dic = {}
+                # dic2 = {}
+                # dic3= {}
+                # for i in range(len(r2.histogram())):
+                #     el = r2.histogram()[i]
+                #     dic[i] = el
+                # for i in range(len(g2.histogram())):
+                #     el = g2.histogram()[i]
+                #     dic2[i] = el
+                # for i in range(len(b2.histogram())):
+                #     el = b2.histogram()[i]
+                #     dic3[i] = el
+                # plt.subplot(3, 1, 1)
+                # plt.bar(list(dic.keys()), dic.values(), color='y')
+                # plt.subplot(3, 1, 2)
+                # plt.bar(list(dic2.keys()), dic2.values(), color='y')
+                # plt.subplot(3, 1, 3)
+                # plt.bar(list(dic3.keys()), dic3.values(), color='y')
+                # plt.show()
+                # endregion
+        except AttributeError:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText("Select image to create histogram")
+            msg.setWindowTitle("Warning!")
+            msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+
+            msg.exec_()
+
+    def check_coloured(self, pix_tuple):
+        counter = 0
+        for i in range(len(pix_tuple)):
+            if pix_tuple[i][0] == pix_tuple[i][1] == pix_tuple[i][2]:
+                counter += 1
+
+        if counter == len(pix_tuple):
+            return False
+
+        return True
+
+    def count_pixels_histogram(self, seq):
+        hist = dict([(x, 0) for x in range(255)])
+        for i in seq:
+            hist[i] = hist.get(i, 0) + 1
+        return hist
+
+    def save_pixel_value(self):
+        if self.clicked_pixel_y != None and self.clicked_pixel_x != None:
+            rgb = self.pixelPhoto.palette().color(QPalette.Background)
+            print("In Save X position: {}, Y position: {}".format(self.clicked_pixel_x, self.clicked_pixel_y))
+            pixmap = self.photo.pixmap()
+            img = pixmap.toImage()
+            img.setPixelColor(int(self.clicked_pixel_x), int(self.clicked_pixel_y), rgb)
+            pix_final = QtGui.QPixmap.fromImage(img)
+            self.photo.setPixmap(pix_final)
+            ImageProcessingWindow.hide()
+            ImageProcessingWindow.show()
+
+    def show_color_dialog(self):
+        selected_color = QColorDialog.getColor()
+        if selected_color.isValid():
+            self.pixelPhoto.setStyleSheet("QLabel { background-color: %s}" % selected_color.name())
+
+    def open_dialog_box(self):
+        filter = "AllFiles (*.jpg *jpeg *.gif *.png *.bmp *.tiff *tif);;JPEG (*.jpg *jpeg);;GIF (*.gif);;PNG(*.png);;BMP (*.bmp);; TIF (*.tiff *.tif)"
+        file = QFileDialog.getOpenFileName(filter=filter)
+        filepath = file[0]
+        pixmap = QPixmap(filepath)
+        self.photo.setPixmap(pixmap)
+        self.pathLabel.setText(filepath)
+        self.size_combo_box()
+
+    def save_dialog_box(self):
+
+        filter = "JPG (*.jpg);;JPEG (*jpeg);;GIF (*.gif);;PNG(*.png);;BMP (*.bmp);; TIF (*.tif);; TIFF(*.tiff)"
+        filename = QFileDialog.getSaveFileName(caption = "Save Image", directory = os.curdir, filter=filter)
+        print(filename)
+        pixmap = self.photo.pixmap()
+        result = pixmap.save(filename[0])
+        print(result)
+
+
+    def size_combo_box(self):
+        index = int(self.sizeBox.currentIndex())
+        # version with quality lose
+
+        #pixmap = QtGui.QPixmap(self.pathLabel.text())
+        # pixmap = self.photo.pixmap()
+        # if index == 0:
+        #     smaller_pixmap = pixmap.scaled(100, 80)
+        #     self.photo.setGeometry(QtCore.QRect(0, 0, 100, 80))
+        #     self.photo.setPixmap(smaller_pixmap)
+        #
+        # elif index == 1:
+        #     smaller_pixmap = pixmap.scaled(200, 160)
+        #     self.photo.setGeometry(QtCore.QRect(0, 0, 200, 160))
+        #     self.photo.setPixmap(smaller_pixmap)
+        # elif index == 2:
+        #     smaller_pixmap = pixmap.scaled(400, 320)
+        #     self.photo.setGeometry(QtCore.QRect(0, 0, 400, 320))
+        #     self.photo.setPixmap(smaller_pixmap)
+        # elif index == 3:
+        #     smaller_pixmap = pixmap.scaled(800, 620)
+        #     self.photo.setGeometry(QtCore.QRect(0, 0, 800, 620))
+        #     self.photo.setPixmap(smaller_pixmap)
+
+
+        # version without quality lose
+        if index == 0:
+            self.photo.setGeometry(QtCore.QRect(0, 0, 100, 80))
+        elif index == 1:
+            self.photo.setGeometry(QtCore.QRect(0, 0, 200, 160))
+
+        elif index == 2:
+            self.photo.setGeometry(QtCore.QRect(0, 0, 400, 320))
+
+        elif index == 3:
+            self.photo.setGeometry(QtCore.QRect(0, 0, 800, 640))
+    def get_pixel_position(self, event):
+        x = event.pos().x()
+        y = event.pos().y()
+        self.clicked_pixel_x = x
+        self.clicked_pixel_y = y
+        print("X position: {}, Y position: {}".format(self.clicked_pixel_x, self.clicked_pixel_y))
+        pixmap = self.photo.pixmap()
+        img = pixmap.toImage()
+        c = img.pixel(x, y)
+        selected_color = QColor(c)
+        self.pixelPhoto.setStyleSheet("QLabel { background-color: %s}" % selected_color.name())
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    ImageProcessingWindow = QtWidgets.QMainWindow()
+    ui = Ui_ImageProcessingWindow()
+    ui.setupUi(ImageProcessingWindow)
+    ImageProcessingWindow.show()
+    sys.exit(app.exec_())
